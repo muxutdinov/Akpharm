9772f8bbb02e5a266ba41048e1f74118b0ae594d
Jaxongir <jmuxutdinov@mail.ru>
2021-11-02 08:18:12 +0500

    body created

--- src/components/Main/MainStyle.js
+++ src/components/Main/MainStyle.js
@@ -2,19 +2,21 @@ import styled from "styled-components";
 import main from "../../assets/imges/main.png";
 
 export const AllWrapper = styled.div`
+  width: 1360px;
   display: flex;
   flex-direction: column;
+  background: #e5e5e5;
 `;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   height: 767px;
-  width: 1440px;
+  /* width: 1440px; */
+  width: 100%;
   background: linear-gradient(90deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.61))
       center center / cover no-repeat,
     url(${main}) rgb(8, 20, 32);
-  background-color: black;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
